7ebcfdd316987f5cc93936cefb2d9efbf17ed8ca
XingruHuang <marioxingru@gmail.com>
2026-02-04 15:55:04 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white text-black">
+      <h1 className="text-5xl font-semibold tracking-tight">Hello World</h1>
+    </div>
+  );
+}
